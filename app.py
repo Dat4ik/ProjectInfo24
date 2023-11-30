6b7b71fdca42d84e9324863e1d60c5203cd68b50
Lunev Andrey <e265.lunev@yandex.com>
2023-11-30 11:53:13 +0300

    Post commit

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import request
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def main_page():
     return render_template('hello.html')
@@ -15,6 +13,10 @@ def hello():
     first_name = request.form['first_name']
     last_name = request.form['second_name']
     name = f'{first_name} {last_name}'
-    return f"Hello, {name}!"
+    if (name == ' '):
+        name = 'Guest'
+    else:
+        name = f'{first_name} {last_name}'
+    return f'Hello, {name}!'
 
 app.run(debug=True)
